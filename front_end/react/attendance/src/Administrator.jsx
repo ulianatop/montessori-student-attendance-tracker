bcf1a9a75c46fa36dd29147989ce01949d75ccbe
Ian Lindquist <ianlindquist@proton.me>
2026-05-16 17:37:34 -0700

    Implemented Administrator.jsx React page and tested. Included administrator.css and a main.tsx for testing it. This is front end implementation for the Admin Tasks page that loads when the Admin Tasks button is pressed on the main page.

---
+++ front_end/react/attendance/src/Administrator.jsx
@@ -0,0 +1,60 @@
+import "./administrator.css";
+import React, {useState} from "react";
+//import { adminTasks } from "./administratortasks";
+
+export default function AdministratorTask(){
+  const [AdminTask, setAdminTask] = useState("");
+  const [result, setResult] = useState("");
+  const adminTasks=() => {
+	   //need logic in here for adminTasks. Probably will remove it since importing this function from a js file (to be implemented still).
+  };  
+	
+	
+return(
+<> 
+<h1>Administrative Tasks</h1>
+
+<div className="center">
+  <div className="boxborder">
+
+    <div className="right">Which task do you want to do?</div>
+
+    <div className="right">
+      <label>Add Student</label>
+      <input type="checkbox" id="addStudent" value="addStudent" />
+    </div>
+
+    <div className="right">
+      <label>Deactivate Student</label>
+      <input type="checkbox" id="deactivateStudent" value="deactivateStudent" />
+    </div>
+
+    <div className="right">
+      <label>Check-in/Check-out Student</label>
+      <input type="checkbox" id="checkInOut" value="checkInOut" />
+    </div>
+
+    <div className="right">
+      <label>Create new admin user</label>
+      <input type="checkbox" id="createUser" value="createUser" />
+    </div>
+
+    <div className="right">
+      <label>Deactivate admin user</label>
+      <input type="checkbox" id="deactivateUser" value="deactivateUser" />
+    </div>
+
+    <div className="right">
+      <label>Run attendance report</label>
+      <input type="checkbox" id="runReport" value="runReport" />
+    </div>
+
+    <div className="buttons">
+      <input type="submit" value="Do Task" />
+    </div>
+
+  </div>
+</div>
+
+</>
+);}
