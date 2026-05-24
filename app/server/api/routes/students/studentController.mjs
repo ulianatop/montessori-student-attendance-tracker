0ea59285c46808f52750dd49193295252f7cb819
Mason <masclearn@gmail.com>
2026-05-23 20:58:19 -0700

    createStudent

--- app/server/api/routes/students/studentController.mjs
+++ app/server/api/routes/students/studentController.mjs
@@ -1,27 +1,31 @@
 // students controller module for handling express request and response objects, logic for students
 import { request, response } from "express";
 import Database from "../../../database.mjs";
-export default class StudentController{
+export default class StudentController {
 
-    constructor(db){
+    constructor(db) {
         this.db = db;
     }
 
-    async getStudents(req, res, next){
+    async getStudents(req, res, next) {
         const students = await this.db.getStudents();
         res.json(students);
         next();
     }
 
-    async getStudentById(req, res, next){
+    async getStudentById(req, res, next) {
         const studentId = req.params["studentId"];
         const student = await this.db.getStudentById(studentId);
         res.json(student);
         next();
     }
 
-    async createStudent(req, res, next){
-        const studentId = req.params["studentId"];
-        
+    async createStudent(req, res, next) {
+        const { StudentFirstName, StudentLastName, AttendanceStatus } = req.body;
+        const insertId = await this.db.createStudent(StudentFirstName, StudentLastName, AttendanceStatus);
+        res.status(200).json({
+            studentId: insertId
+        })
+        next();
     }
 }
