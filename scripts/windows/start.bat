6e7953e9903a3d7fb353eb965277edff15355366
Electricjacob11 <electricjacob11@gmail.com>
2026-05-29 21:31:24 -0700

    Startup windows script cleanup

--- scripts/windows/start.bat
+++ scripts/windows/start.bat
@@ -1,6 +1,5 @@
 @echo off
-START "" cmd /k "cd /D ..\..\app\server\ && node server.mjs"
-START "" cmd /k "cd /D ..\..\app\client\react\attendance && npm install html5-qrcode qrcode && npm run dev"
-@REM START "" cmd /k "cd /D ..\..\app\client\react\attendance && npm run dev"
+START "" cmd /k "cd /D ..\..\app\client\react\attendance && npm install html5-qrcode qrcode"
+START "" cmd /k "cd /D ..\..\app\ && npm run dev"
 timeout /t 3 >nul
 START "" http://localhost:5173
