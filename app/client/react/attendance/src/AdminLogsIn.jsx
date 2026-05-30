82bf394d42a11a8e66e52d97511525e633c4c8f6
xerrkel <IanLindquist@proton.me>
2026-05-30 09:54:33 -0700

    added comments to AdminLogsIn.jsx (#29)
    Authored-by: Ian Lindquist <ianlindquist@pm.me>

--- app/client/react/attendance/src/AdminLogsIn.jsx
+++ app/client/react/attendance/src/AdminLogsIn.jsx
@@ -1,31 +1,29 @@
-import React, {useState} from "react";
-import { useNavigate } from 'react-router-dom';
-import "./administrator.css";
-import { checkPassword } from "./adminloginhandler";
-export default function AdminLogsIn(){
-	const [loginMessage, setLoginMessage] = useState("");
-	const [adminName, setAdminName] = useState("");
-	const [adminPassword, setAdminPassword] = useState("");
-	const [errorMessage, setErrorMessage] = useState('');
-	 const [result, setResult] = useState("");
-	const navigate = useNavigate();
+import React, {useState} from "react"; // import the React library, useState hook to track input field changes.
+import { useNavigate } from 'react-router-dom'; // Import navigation hook from react-router-dom
+import "./administrator.css"; //import CSS
+import { checkPassword } from "./adminloginhandler"; //import password checking helper function
+export default function AdminLogsIn(){ // declare main function AdminLogsIn
+	const [loginMessage, setLoginMessage] = useState(""); // init state viariable for login messages or errors
+	const [adminName, setAdminName] = useState(""); // init state variable for username input
+	const [adminPassword, setAdminPassword] = useState(""); // init state variable for password input
+	const navigate = useNavigate(); // init nav function
 
-const handleLoginClick = async () => {
+const handleLoginClick = async () => { // trigger for when user clicks Login button
     try {
-        setLoginMessage(""); 
-        const message = await checkPassword(adminName, adminPassword);
-        setLoginMessage(message); 
-		if (message === "Login successful!") {
+        setLoginMessage(""); //Clear old login messages 
+        const message = await checkPassword(adminName, adminPassword); // call the API function to check username & password
+        setLoginMessage(message);  // store the reply from the server
+		if (message === "Login successful!") { //if success, wait one then redirect to admin tasks API
 			setTimeout(() => {
                 navigate('/admin-tasks');
             }, 1000);
 		}
     } catch (err) {     
-        setLoginMessage(err.message || "An error occurred during login.");
+        setLoginMessage(err.message || "An error occurred during login."); //catch any errors during login
     }
 };
 
-return(
+return( // return the UI stuff
 <>
 <br /><br /><br /><br /><br />
 	<h1>Administrator Login Page</h1>
